c6a958f009be34c91a778369106821acee9e6b52
Chris Rodriguez <Crodriguez4443@gmail.com>
2026-05-05 12:14:27 -0400

    new blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://dendearts.com',
   integrations: [
     sitemap(),
+    mdx(),
   ],
   output: 'static',
   image: {
